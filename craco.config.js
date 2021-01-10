04428c10f2ec24be7f977d290f140dd3a618bb09
Stanislav Klymoshenko <stanislav.klymoshenko@easysoftware.com>
2021-01-10 15:10:48 +0100

    test publish set up 2

--- craco.config.js
+++ craco.config.js
@@ -6,5 +6,28 @@ module.exports = {
         require("autoprefixer")
       ]
     }
+  },
+  webpack: {
+    entry: ["babel-polyfill", "./src/index.ts"],
+    output: {
+      path: __dirname + "/src/build/",
+      filename: "index.js"
+    },
+    resolve: {
+      extensions: ["", ".js", ".jsx", ".tsx", ".ts"]
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loader: "babel",
+          exclude: /node_modules/,
+          query: {
+            cacheDirectory: true,
+            presets: ["react", "es2015"]
+          }
+        }
+      ]
+    }
   }
 };
